05266ec83448425baef2e714778f873b9c5d50c4
Charles Reynolds <reynoldscem@gmail.com>
2016-12-30 14:15:07 +0000

    Early stopping, param saving

--- log_reg.py
+++ log_reg.py
@@ -35,8 +35,18 @@ class LogisticRegression():
             T.clip(target_prob, 0.001, 0.999)
         ).mean() + lbd * T.sum(self.W ** 2)
 
-    def mean_abs_err(self, target_prob):
-        return T.mean(abs(self.pred_prob - target_prob))
+    def mean_abs_err(self, target_prob, lbd=0.):
+        return T.mean(abs(
+            self.pred_prob - target_prob
+        )) + lbd * T.sum(self.W ** 2)
+
+    def mean_squared_err(self, target_prob, lbd=0.):
+        return T.mean((
+            self.pred_prob - target_prob
+        ) ** 2) + lbd * T.sum(self.W ** 2)
+
+    def get_param_values(self):
+        return [param.get_value() for param in self.params]
 
 
 def load_data():
@@ -49,7 +59,7 @@ def load_data():
     return trainX, trainy, testX, testy
 
 
-def get_train_test_functions(learning_rate, n_features):
+def get_model_and_funcs(learning_rate, n_features):
     X = T.matrix('X', dtype=theano.config.floatX)
     y = T.matrix('y', dtype=theano.config.floatX)
 
@@ -58,7 +68,9 @@ def get_train_test_functions(learning_rate, n_features):
         n_in=n_features
     )
 
-    loss = model.binary_cross_entropy(y)
+    # loss = model.binary_cross_entropy(y)
+    loss = model.mean_abs_err(y, 5e-2)
+    #loss = model.mean_squared_err(y, 5e-2)
     grads = T.grad(loss, model.params)
     updates = nesterov_momentum(grads, model.params, learning_rate)
 
@@ -78,7 +90,7 @@ def get_train_test_functions(learning_rate, n_features):
         model.binary_cross_entropy(y, 0.)
     )
 
-    return train_model, test_model, model_loss
+    return train_model, test_model, model_loss, model
 
 
 def train_val_split(trainX, trainy, split_degree=0.9, shuffle=True):
@@ -102,20 +114,22 @@ def train_val_split(trainX, trainy, split_degree=0.9, shuffle=True):
 def main(args):
     learning_rate = theano.shared(np.array(1e-2, dtype=theano.config.floatX))
     decay_frequency = 25
-    anneal_factor = 0.5
+    anneal_factor = 0.9
     min_decay = 25
     n_epochs = 1000
 
     def decay_function(epoch):
         if epoch >= min_decay and epoch % decay_frequency == 0:
-            new_lr = learning_rate.get_value() * anneal_factor
+            new_lr = (
+                learning_rate.get_value() * anneal_factor
+            ).astype(theano.config.floatX)
             print('Learning rate annealing to {:.5f}'.format(new_lr))
             learning_rate.set_value(new_lr)
 
     trainX, trainy, testX, testy = load_data()
     trainX, trainy, valX, valy = train_val_split(trainX, trainy)
 
-    train_function, test_function, model_loss = get_train_test_functions(
+    train_function, test_function, model_loss, model = get_model_and_funcs(
         learning_rate, trainX.shape[-1]
     )
 
@@ -124,15 +138,28 @@ def main(args):
         'train loss {:.8f}, val loss {:.8f}, val err {:.4f}'
     )
 
+    best_val_error = np.inf
+    epochs_since_best = 0
+
     for epoch in range(1, n_epochs + 1):
         start_time = time.process_time()
 
         decay_function(epoch)
 
         epoch_loss = float(train_function(trainX, trainy))
+        epochs_since_best += 1
 
         val_loss = float(model_loss(valX, valy))
         val_error = float(test_function(valX, valy))
+
+        if val_error <= best_val_error:
+            best_val_error = val_error
+            epochs_since_best = 0
+            best_params = model.get_param_values()
+
+        if epochs_since_best >= 25:
+            print('Early stopping')
+            break
 
         time_delta = time.process_time() - start_time
         print(output_format_string.format(
@@ -141,6 +168,7 @@ def main(args):
 
     test_error = float(test_function(testX, testy))
     print('Finished, test MAE: {}'.format(test_error))
+    np.savez('parameters_{}.npz'.format(int(time.time()), *best_params))
 
 
 if __name__ == '__main__':
